7e7e42e308e1144ecb8bebfbf8d83148aa563abe
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-05-22 07:17:49 +0600

    A new model named Story have been registered

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post,Comment, About
+from .models import Post,Comment, About, Story
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(About)
+admin.site.register(Story)
