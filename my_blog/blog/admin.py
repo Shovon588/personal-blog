dd5b2b1a5609b5f1c7d7cd79e1417308f642794e
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-05-23 23:13:30 +0600

    New Model reader info registered

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Post,Comment, About, Story
+from .models import Post,Comment, About, Story, ReaderInfo
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(About)
 admin.site.register(Story)
+admin.site.register(ReaderInfo)
+
